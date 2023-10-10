532e0c3e021ebe58acdada33ca3976c89746aaa5
hassanshahzadse <hassanshahzadvs@gmail.com>
2023-10-11 00:34:43 +0500

    added login and signup page changes

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,130 +1,294 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import * as Yup from 'yup'
-import './login.css'
-import BASE_URL from '../../services/api'
-
-
-
-const Login = () => {
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import BASE_URL from "../../services/api";
+import "./login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLock,
+  faPerson,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+const Auth = () => {
   const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("login");
+
   const initialValues = {
-    email: '',
-    password: '',
-  }
-  const [togglePassword, setTogglePassword] = useState(false)
+    email: "",
+    password: "",
+    firstName: "",
+  };
 
-  const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Email is required'),
-  })
+  const [togglePassword, setTogglePassword] = useState(false);
 
-  const handleSubmit = async values => {
+  const handleSubmit = async (values) => {
+    console.log("Login button clicked");
     try {
-      const response = await fetch(`${BASE_URL}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: values.email,
-          password: values.password,
-        }),
-      })
+      const response = await axios.post(`${BASE_URL}/auth/login`, {
+        email: values.email,
+        password: values.password,
+      });
 
-      if (!response.ok) {
-        // Check for non-2xx HTTP response status
-        const errorResponseData = await response.json()
-        throw new Error(errorResponseData.message || 'An error occurred')
+      if (response.status === 200) {
+        const responseData = response.data;
+        // Store token and other information in local storage
+        localStorage.setItem("Bluebottoken", responseData.token);
+        localStorage.setItem("role", responseData.role);
+        localStorage.setItem("email", responseData.email);
+        localStorage.setItem("userName", responseData.username);
+        localStorage.setItem("UserId", responseData.UserId);
+        navigate("/dashboard");
+      } else {
+        throw new Error("Login failed");
       }
-      
-      const responseData = await response.json()
-      console.log(responseData)
-      // Store token and other information in local storage
-      localStorage.setItem('Bluebottoken', responseData.token)
-      localStorage.setItem('role', responseData.role)
-      localStorage.setItem('email', responseData.email)
-      localStorage.setItem('userName', responseData.username)
-      localStorage.setItem('UserId', responseData.UserId)
-      navigate('/dashboard')
-      // window.alert('Logged In Successfully')
-      // window.location.href = '/dashboard'
-      // window.location.href = '/socialProfile'
-
-      // Redirect to dashboard or any other protected route
     } catch (error) {
-      // Handle login error
-      window.alert(error.message || 'An error occurred during login')
+      window.alert(error.message || "An error occurred during login");
     }
-  }
+  };
+
+  const handleSignupSubmit = async (values) => {
+    console.log("Signup button clicked");
+    try {
+      const response = await axios.post(`${BASE_URL}/auth/signup`, values);
+        window.alert("Successfully Signed Up");
+        setActiveTab("login");
+    } catch (error) {
+      window.alert(error.message || "An error occurred during SignUp");
+    }
+  };
+
+  const switchToLogin = () => {
+    setActiveTab("login");
+  };
+
+  const switchToSignup = () => {
+    setActiveTab("signup");
+  };
 
   return (
     <>
-      <div className="main-container-login">
-        <div className="container-fluid d-flex align-items-center justify-content-between">
-          <div className="d-flex align-items-center">
-            <img src="/images/robot1.png" alt="Robot" className="robot-image" />
-            <h4 className="logo-heading1">BLUEBOT SOCIAL</h4>
+      <div className="main-container-login display-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex w-100 justify-content-center align-items-center">
+          <div className="auth-tabs alig-items-center">
+            <img src="/Images/robot1.png" alt="Robot" className="-image" />
+            <button
+              className={`mt-3 mb-3 auth-tab ${
+                activeTab === "login" ? "active" : ""
+              }`}
+              onClick={switchToLogin}
+            >
+              <div className="d-flex flex-column align-items-center p-3 switch-tabs-auth">
+                <i
+                  className="fas fa-user mb-2"
+                  style={{ fontSize: "24px" }}
+                ></i>
+
+                <p className="m-0">Login</p>
+              </div>
+            </button>
+            <button
+              className={`mt-3 mb-3 auth-tab ${
+                activeTab === "signup" ? "active" : ""
+              }`}
+              onClick={switchToSignup}
+            >
+              <div className="d-flex flex-column align-items-center p-2 switch-tabs-auth">
+                <i
+                  className="fas fa-user mb-2"
+                  style={{ fontSize: "24px" }}
+                ></i>
+
+                <p className="m-0">SignUp</p>
+              </div>
+            </button>
           </div>
-          <div className="d-flex align-items-center">
-            {/* <button className="btn custom-buttonn me-2">add post</button> */}
-            <div className="d-flex settings-button-container">
-              <img className="me-2" src="/images/das.png" alt="Dashboard" />
-              <img className="me-2" src="/images/noti.png" alt="Notifications" />
-              <img className="me-2" src="/images/setting.png" alt="Profile" />
-              <img className="" src="/images/pro.png" alt="Settings" />
-            </div>
-          </div>
-        </div>
-        <div className="login-container">
-          <div className="card col-md-4">
-            <h2 className="text-center">Sign in to Continue </h2>
-            <div className="card-body">
-              <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                <Form>
-                  <div className="form-group">
-                    <label htmlFor="email ">Email</label>
-                    <Field type="email" className="form-control mt-2" placeholder="Enter email address" id="email" name="email" required />
-                    <ErrorMessage name="email" component="div" className="alert text-danger" />
-                  </div>
-                  <div className="form-group mt-4">
-                    <label htmlFor="password ">Password</label>
-                    <Field type={togglePassword ? 'text' : 'password'} className="form-control mt-2" placeholder="Enter password" id="password" name="password" required />
-                    <div
-                      style={{
-                        display: 'flex',
-                        width: '100%',
-                        justifyContent: 'flex-end',
-                        marginTop: '0.3rem',
-                      }}
-                    >
-                      <span
-                        style={{ color: 'skyblue', textDecoration: 'underline', cursor: 'pointer' }}
-                        onClick={() => {
-                          setTogglePassword(!togglePassword)
+
+          {activeTab === "login" && (
+            <div className="login-container scroll-animation">
+              <img
+                src="/Images/loginPeople.png"
+                alt="Image 2"
+                className="img-container"
+              />
+              <div className="card">
+                <h1 className="no-account">
+                  Don't have an account?{" "}
+                  <span onClick={switchToSignup}>Sign Up.</span>
+                </h1>
+                <Formik
+                  initialValues={initialValues}
+                  // validationSchema={validationSchema}
+                  onSubmit={handleSubmit}
+                >
+                  <Form>
+                    <div className="form-container">
+                      <label htmlFor="email">E-Mail</label>
+                      <div className="input-container">
+                        <Field
+                          type="email"
+                          className="field-class"
+                          placeholder="youremail@gmail.com"
+                          id="email"
+                          name="email"
+                          required
+                        />
+                        <FontAwesomeIcon
+                          icon={faEnvelope}
+                          className="email-icon"
+                        />
+                      </div>
+                      <div className="error">
+                        <ErrorMessage name="email" component="div" />
+                      </div>
+                    </div>
+                    <div className="form-container">
+                      <label htmlFor="password ">Password</label>
+                      <div className="input-container">
+                        <Field
+                          type="password"
+                          className="field-class"
+                          placeholder="Enter password"
+                          id="password"
+                          name="password"
+                          required
+                        />
+                        <FontAwesomeIcon icon={faLock} className="email-icon" />
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                          width: "100%",
                         }}
                       >
-                        Show Password
-                      </span>
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className="error"
+                        />
+                      </div>
                     </div>
-                    <ErrorMessage name="password" component="div" className="alert text-danger" />
-                  </div>
-                  <button type="submit" to="/sidebar" className="btn mt-5 custom-buttonn">
-                    Login
-                  </button>
-                </Form>
-              </Formik>
-              <hr />
-              <Link to="/signup" className="button btn custom-buttonn">
-                Don't have an account? Signup
-              </Link>
+                    <button type="submit" to="/sidebar" className="submit-btn">
+                      SIGN IN
+                    </button>
+                    <h1 className="privacy">
+                      By cLicking the Sign In Button, you therefore agree to the
+                      Privacy Policy.
+                      <br className="span-priv" />
+                      For more information, read about our privacy policy{" "}
+                      <span className="span-priv-link"><Link to='/privacyPolicy'>here</Link></span>
+                    </h1>
+                  </Form>
+                </Formik>
+              </div>
             </div>
-          </div>
+          )}
+
+          {activeTab === "signup" && (
+            <div className="login-container scroll-animation">
+              <img
+                src="/Images/signUpPeople.jpg"
+                alt="Image 2"
+                className="img-fluid"
+              />
+              <div className="card">
+                <h1 className="no-account">
+                  Already have an account?{" "}
+                  <span onClick={switchToLogin}>Sign In.</span>
+                </h1>
+                <Formik
+                  initialValues={initialValues}
+                  // validationSchema={validationSchema}
+                  onSubmit={handleSignupSubmit}
+                >
+                  <Form>
+                    <div className="form-container">
+                      <label htmlFor="firstName">Username</label>
+                      <div className="input-container">
+                        <Field
+                          type="text"
+                          className="field-class"
+                          placeholder="myusername"
+                          id="firstName"
+                          name="firstName"
+                        />
+                        <FontAwesomeIcon icon={faUser} className="email-icon" />
+                      </div>
+                      <ErrorMessage
+                        name="firstName"
+                        component="div"
+                        className="error"
+                      />
+                    </div>
+                    <div className="form-container">
+                      <label htmlFor="email">E-Mail</label>
+                      <div className="input-container">
+                        <Field
+                          type="email"
+                          className="field-class"
+                          placeholder="youremail@gmail.com"
+                          id="email"
+                          name="email"
+                          required
+                        />
+                        <FontAwesomeIcon
+                          icon={faEnvelope}
+                          className="email-icon"
+                        />
+                      </div>
+                      <div className="error">
+                        <ErrorMessage name="email" component="div" />
+                      </div>
+                    </div>
+                    <div className="form-container">
+                      <label htmlFor="password ">Password</label>
+                      <div className="input-container">
+                        <Field
+                          type={togglePassword ? "text" : "password"}
+                          className="field-class"
+                          placeholder="Enter password"
+                          id="password"
+                          name="password"
+                          required
+                        />
+                        <FontAwesomeIcon icon={faLock} className="email-icon" />
+                      </div>
+                      <div className="show-password">
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className="error"
+                        />
+                        <span
+                          style={{
+                            color: "skyblue",
+                            textDecoration: "underline",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => {
+                            setTogglePassword(!togglePassword);
+                          }}
+                        >
+                          Show Password
+                        </span>
+                      </div>
+                    </div>
+
+                    <button type="submit" className="submit-btn">
+                      Sign Up
+                    </button>
+                  </Form>
+                </Formik>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Auth;
--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,7 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import FinalContentBox from '../Component/Dashboard/CoverContentBox/FinalContentBox';
 import Login from '../Pages/Login';
-import SignUp from '../Pages/Signup';
 import Workspace from '../Component/Workplace/CoverBoxofWorkplace/Workspace';
 import Sidebar from '../Component/Sidebar/Sidebar';
 import SocialProfile from '../Pages/socialProfile';
@@ -19,7 +18,6 @@ const AppRoutes = () => {
       <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Sidebar />}>
         {
           isAuthenticatedUser ? (
