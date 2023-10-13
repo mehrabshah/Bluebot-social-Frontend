1fc448d57e18e246eac0f94c0ed63a04b0bc958e
hassanshahzadse <hassanshahzadvs@gmail.com>
2023-10-13 14:25:07 +0500

    new build

--- src/services/redirect.js
+++ src/services/redirect.js
@@ -1,4 +1,4 @@
 // const REDIRECT_URL = 'http://localhost:8000/dashboard';
-const REDIRECT_URL = 'https://app.bluebotsocial.com/dashboard';
+const REDIRECT_URL = 'https://bluebotsocial.com/dashboard';
 
 export default REDIRECT_URL;
